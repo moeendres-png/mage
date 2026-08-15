837bc92520c0aed8b72fbc041b167f2cbe0c860a
Moe <moe.endres@gmail.com>
2026-08-15 18:10:29 +0200

    feat: unprepare permanent when prepare spell is cast

--- Mage/src/main/java/mage/cards/PrepareSpellCard.java
+++ Mage/src/main/java/mage/cards/PrepareSpellCard.java
@@ -1,9 +1,13 @@
 package mage.cards;
 
+import mage.MageObjectReference;
 import mage.abilities.SpellAbility;
 import mage.abilities.costs.mana.ManaCostsImpl;
 import mage.constants.CardType;
 import mage.constants.SpellAbilityType;
+import mage.constants.Zone;
+import mage.game.Game;
+import mage.game.permanent.Permanent;
 
 import java.util.UUID;
 
@@ -15,10 +19,24 @@ public class PrepareSpellCard extends CardImpl {
 
     private PrepareCard prepareCardParent;
 
-    public PrepareSpellCard(UUID ownerId, CardSetInfo setInfo, String preparationName, CardType[] cardTypes, String costs, PrepareCard prepareCardParent) {
-        super(ownerId, setInfo, cardTypes, costs, SpellAbilityType.BASE_ALTERNATE);
+    public PrepareSpellCard(
+            UUID ownerId,
+            CardSetInfo setInfo,
+            String preparationName,
+            CardType[] cardTypes,
+            String costs,
+            PrepareCard prepareCardParent
+    ) {
+        super(
+                ownerId,
+                setInfo,
+                cardTypes,
+                costs,
+                SpellAbilityType.BASE_ALTERNATE
+        );
 
-        SpellAbility newSpellAbility = new SpellAbility(new ManaCostsImpl<>(costs), preparationName);
+        SpellAbility newSpellAbility =
+                new SpellAbility(new ManaCostsImpl<>(costs), preparationName);
         this.replaceSpellAbility(newSpellAbility);
         spellAbility = newSpellAbility;
 
@@ -34,5 +52,32 @@ public class PrepareSpellCard extends CardImpl {
     @Override
     public PrepareSpellCard copy() {
         return new PrepareSpellCard(this);
+    }
+
+    @Override
+    public boolean cast(
+            Game game,
+            Zone fromZone,
+            SpellAbility ability,
+            UUID controllerId
+    ) {
+        if (!super.cast(game, fromZone, ability, controllerId)) {
+            return false;
+        }
+
+        Object prepareRef = game.getState().getValue(
+                PrepareCard.getPrepareCopyKey(getId())
+        );
+
+        if (prepareRef instanceof MageObjectReference) {
+            Permanent preparedPermanent =
+                    ((MageObjectReference) prepareRef).getPermanent(game);
+
+            if (preparedPermanent != null && preparedPermanent.isPrepared()) {
+                preparedPermanent.setPrepared(false, game);
+            }
+        }
+
+        return true;
     }
 }
